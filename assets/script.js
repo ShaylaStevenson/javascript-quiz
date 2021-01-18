ce104e9b9e41929f556dfdfe2203c62c903e28b4
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-01-17 17:59:42 -0800

    added start quiz button and ready function

--- assets/script.js
+++ assets/script.js
@@ -10,127 +10,172 @@
 //Create scoreboard to view results, set with users initials and display
 //Get the quiz to load
 
-//variable to record the progress of the quiz ex : Q3 of 4
-var progress = 0;
 
-//variable to record how many correct answers user has
-var correct = 0;
 
-//variable to contain the "test" <div>
-var testEl;
 
-//variable to contain the header that displays progress
-var progressEl;
+//Function to prevent JS running until DOM loaded fully
+$(document).ready(function() {
 
-//variable to contain a question grabbed from an array
-var question;
-
-//variable to record the user's selected choice
-var userChoice;
-
-//variable to contain the possible answers displayed for the user
-var choices;
-
-//variables to contain each possible answer
-var chA, chB, chC;
-
-//Arrays within larger array, where questions with answers are stored
-var possibleQuestions = [
-    {
-        question: "I am a question?",
-        a: "I am a pickle.",
-        b: "I am a Rick.",
-        c: "I am am Morty.",
-        answer: "B"
-    },
-    {
-        question: "Hey, I'm a question, too!?",
-        a: "I am a burrito",
-        b: "I am a taco",
-        c: "I am Zorro",
-        answer: "C",
-    },
-    {
-        question: "I am a question?",
-        a: "I am a pickle.",
-        b: "I am a Rick.",
-        c: "I am am Morty.",
-        answer: "B"
-    },
-    {
-        question: "Hey, I'm a question, too!?",
-        a: "I am a burrito",
-        b: "I am a taco",
-        c: "I am Zorro",
-        answer: "C",
-    }
-];
-
-//get() to reduce keystrokes
-function get(x) {
-    return document.getElementById(x);
-}
-
-//function to display a question on the screen
-function displayQuestion() {
-    testEl = get("testEl");
-    if (progress >= possibleQuestions.length) {
-        testEl.innerHTML = "<h2>You answered " + correct + " out of " + possibleQuestions.length + " questions correctly.<h2>";
-        get("progressEl").innerHTML = "You have completed the quiz.";
-        //reset variables to reset quiz
-        progress = 0;
-        correct = 0;
-        //stops function
-        return false;
-    }
-
-    //displays progress of quiz in progressEl
-    get("progressEl").innerHTML = "Question " + (progress+1) + " of " + possibleQuestions.length;
-
-    //"assign" keys from questins in possible questions array to variables, which can be reused
-    question = possibleQuestions[progress].question;
-    chA = possibleQuestions[progress].a;
-    chB = possibleQuestions[progress].b;
-    chC = possibleQuestions[progress].c;
-
-    //display the question
-    testEl.innerHTML = "<h3>" + question + "</h3>";
-
-    //display the multiple-choices
-    //??recreate by hooking to elemnets instead of creating in JS?
-    testEl.innerHTML += "<label><input type='radio' name='choices' value='A'>" + chA + "</label><br>";
-    testEl.innerHTML += "<label><input type='radio' name='choices' value='A'>" + chB + "</label><br>";
-    testEl.innerHTML += "<label><input type='radio' name='choices' value='A'>" + chC + "</label><br>";
-
-    //show submit button when answer is selected
-    testEl.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-}
-
-//function to check answers
-function checkAnswer() {
-    choices = document.getElementsByName("choices");
     
-    //loop through choices and stops at checked choice
-    for (var i = 0; i < choices.length; i++) {
-        if (choices[i].checked) {
-            userChoice = choices[i].value;
+
+    //variable to contain start quiz button
+    var startQuizBtn = document.getElementById("startQuizBtn");
+
+    //variable to contain the "test" <div>
+    var testEl;
+
+    //variable to contain the header that displays progress numerically
+    var progressEl;
+
+    //variable to contain visual representation of progress of quiz via a bar
+    var progressBarEl;
+
+    //variable to display the seconds left for quiz
+    var timerEl;
+
+    //variable to display highscores with initials
+    var scoreboardEl;
+
+    //variable to record the progress of the quiz ex : Q3 of 4
+    var progress = 0;
+
+    //variable to record how many correct answers user has
+    var correct = 0;
+
+    //variable to contain a question grabbed from an array
+    var question;
+
+    //variable to record the user's selected choice
+    var userChoice;
+
+    //variable to contain the possible answers displayed for the user
+    var choices;
+
+    //variables to contain each possible answer
+    var chA, chB, chC;
+
+    
+    //Arrays within larger array, where questions with answers are stored
+    var possibleQuestions = [
+        {
+            question: "I am a question?",
+            a: "I am a pickle.",
+            b: "I am a Rick.",
+            c: "I am am Morty.",
+            answer: "B"
+        },
+        {
+            question: "Hey, I'm a question, too!?",
+            a: "I am a burrito",
+            b: "I am a taco",
+            c: "I am Zorro",
+            answer: "C",
+        },
+        {
+            question: "I am a question?",
+            a: "I am a pickle.",
+            b: "I am a Rick.",
+            c: "I am am Morty.",
+            answer: "B"
+        },
+        {
+            question: "Hey, I'm a question, too!?",
+            a: "I am a burrito",
+            b: "I am a taco",
+            c: "I am Zorro",
+            answer: "C",
         }
+    ];
+
+    //on whether a checkbox has been ticked or not.
+    //function confirmAnswer(){
+    //    //If the checkbox has been checked
+    //    if(chA.checked || chB.checked || chC.checked){
+    //        //Set the disabled property to FALSE and enable the button.
+    //        document.get("submitAnswerBtn").disabled = false;
+    //        submitAnswerBtn.addEventListener("click", checkAnswer);
+    //    } else{
+    //        //Otherwise, disable the submit button.
+    //        document.get("submitAnswerBtn").disabled = true;
+    //    }
+
+
+
+    //get() to reduce keystrokes
+    //Don't move! Will stop working!
+    function get(x) {
+        return document.getElementById(x);
     }
-    //check if userChoice matches answer key stored in array
-    if (userChoice == possibleQuestions[progress].answer) {
-        correct++;
+
+    
+    //function to display a question on the screen
+    function displayQuestion() {
+        testEl = get("testEl");
+        if (progress >= possibleQuestions.length) {
+            testEl.innerHTML = "<h2>You answered " + correct + " out of " + possibleQuestions.length + " questions correctly.<h2>";
+            get("progressEl").innerHTML = "You have completed the quiz.";
+            //reset variables to reset quiz
+            progress = 0;
+            correct = 0;
+            //stops function
+            return false;
+        }
+
+        //displays progress of quiz in progressEl
+        get("progressEl").innerHTML = "Question " + (progress+1) + " of " + possibleQuestions.length;
+
+        //"assign" keys from questions in possible questions array to variables, which can be reused
+        question = possibleQuestions[progress].question;
+        chA = possibleQuestions[progress].a;
+        chB = possibleQuestions[progress].b;
+        chC = possibleQuestions[progress].c;
+
+        //display the question
+        testEl.innerHTML = "<h3>" + question + "</h3>";
+
+        //display the multiple-choices
+        //??recreate by hooking to elemnets instead of creating in JS?
+        testEl.innerHTML += "<label><input type='radio' name='choices' value='A'>" + chA + "</label><br>";
+        testEl.innerHTML += "<label><input type='radio' name='choices' value='B'>" + chB + "</label><br>";
+        testEl.innerHTML += "<label><input type='radio' name='choices' value='C'>" + chC + "</label><br>";
+       
+        //confirmAnswer();
+
+        //submit button when answer is selected which will render checkAnswer function
+        submitAnswerBtn.addEventListener("click", checkAnswer);
+    }  
+
+    //function to check answers
+    function checkAnswer() {
+        choices = document.getElementsByName("choices");
+        
+        //loop through choices and stops at checked choice
+        for (var i = 0; i < choices.length; i++) {
+            if (choices[i].checked) {
+                userChoice = choices[i].value;
+            }
+        }
+        //check if userChoice matches answer key stored in array
+        if (userChoice == possibleQuestions[progress].answer) {
+            correct++;
+        }
+        //increases progress and displays next question
+        progress++;
+        displayQuestion();
     }
-    //increases progress and displays next question
-    progress++;
-    displayQuestion();
-}
 
 //add event listener to load quiz when page opens
 //??recreate so that quiz starts when button clicked??
-window.addEventListener("load", displayQuestion);
+//startQuizBtn.addEventListener("click", displayQuestion);
+//startQuizBtn.addEventListener("click", runQuiz);
+
+  
+startQuizBtn.addEventListener("click", displayQuestion);
+});
 
 //collect users initials for score board
 //in html create scorebard
+//store scores in an object array, sort array, and display in scoreboard,local storage and json?
 //add restart button
 //add timer that starts onclick of begin button
 //end game when timer = 0
